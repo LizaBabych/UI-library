f6f8f62e8c52541e910eda91911aba0c876f2638
LizaBabych <62441776+LizaBabych@users.noreply.github.com>
2020-06-07 20:41:25 +0300

    Change solution.

--- js/carousel.js
+++ js/carousel.js
@@ -1,13 +1,28 @@
-let slideNum = 1;
-let carouselNumber = document.getElementById("carousel");
+let carouselNumber = document.getElementsByClassName("slider");
 
-function plusSlides(n) {
-  showSlides(slideNum += n, carouselNumber);
+for (let i = 0; i < carouselNumber.length; i++) {
+  let slides = carouselNumber[i].getElementsByClassName("slid");
+  let slideNum = 1;
+
+  showSlides(slideNum, carouselNumber, slides);
+
+  let prevButton = carouselNumber[i].getElementsByClassName("prev");
+  let nextButton = carouselNumber[i].getElementsByClassName("next");
+
+  for (let j = 0; j < nextButton.length; j++) {
+    nextButton[j].addEventListener('click', function(){
+      showSlides(slideNum += 1, carouselNumber[i], slides)
+    });
+  }
+
+  for (let j = 0; j < prevButton.length; j++) {
+    prevButton[j].addEventListener('click', function(){
+      showSlides(slideNum -= 1, carouselNumber[i], slides)
+    });
+  }
 }
-showSlides(slideNum, carouselNumber);
 
-function showSlides(n, carouselNum) {
-  let slides = carouselNum.getElementsByClassName("slid");
+function showSlides(slideNum, carouselNum, slides) {
   for(let i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
