dbbebc84a0cd828e73f0ed08530e9d2fd99621e8
LizaBabych <62441776+LizaBabych@users.noreply.github.com>
2020-06-10 21:31:35 +0300

    Add querySelectorAll and forEach.

--- js/modal.js
+++ js/modal.js
@@ -1,20 +1,20 @@
-const send = document.getElementsByClassName("modal-trigger");
-const closeModal = document.getElementsByClassName("modal-close");
+const send = document.querySelectorAll(".modal-trigger");
+const closeModal = document.querySelectorAll(".modal-close");
 let newButton;
 
-for (let i = 0; i < send.length; i++) {
-  send[i].addEventListener('click', function() {
-    send[i].nextElementSibling.style.display = "block";
+send.forEach((item, i) => {
+  item.addEventListener('click', function() {
+    item.nextElementSibling.style.display = "block";
     newButton = document.createElement("button");
     newButton.textContent = "X";
-    send[i].nextElementSibling.firstElementChild.firstElementChild.appendChild(newButton);
+    item.nextElementSibling.firstElementChild.firstElementChild.appendChild(newButton);
     newButton.addEventListener('click', function(){closeModalWindow(closeModal[i])});
   });
-}
+});
 
-for (let i = 0; i < closeModal.length; i++) {
-  closeModal[i].addEventListener('click', function(){closeModalWindow(closeModal[i])});
-}
+closeModal.forEach((item, i) => {
+  item.addEventListener('click', function(){closeModalWindow(closeModal[i])});
+});
 
 function closeModalWindow(id) {
   id.parentElement.parentElement.parentElement.style.display = "none";
