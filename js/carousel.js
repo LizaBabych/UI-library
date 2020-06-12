08829aff71f57bcb18161a53fc6a7336996c7983
LizaBabych <62441776+LizaBabych@users.noreply.github.com>
2020-06-12 11:48:35 +0300

    Change desicion.

--- js/carousel.js
+++ js/carousel.js
@@ -1,30 +1,58 @@
-let carouselNumber = document.querySelectorAll(".slider");
+const config = ["https://loremflickr.com/320/240/dog", "https://loremflickr.com/g/320/240/paris",
+ "https://loremflickr.com/320/240/paris,girl/all", "https://loremflickr.com/320/240/dog", "https://loremflickr.com/g/320/240/paris",
+ "https://loremflickr.com/320/240/paris,girl/all", "https://loremflickr.com/320/240/dog", "https://loremflickr.com/g/320/240/paris",
+ "https://loremflickr.com/320/240/paris,girl/all"];
 
-carouselNumber.forEach((item, i) => {
-  let slides = item.querySelectorAll(".slid");
+carousel("slider", config);
+
+function carousel(id, config) {
+  let carousel = document.getElementById(id);
+  let slide, img;
   let slideNum = 1;
-
-  showSlides(slideNum, carouselNumber, slides);
-
-  let prevButton = item.querySelectorAll(".prev");
-  let nextButton = item.querySelectorAll(".next");
-
-  nextButton.forEach((itemNextButton) => {
-    itemNextButton.addEventListener('click', function(){
-      showSlides(slideNum += 1, item, slides)
-    });
+  let slides = [];
+  config.forEach((item, i) => {
+    slide = document.createElement('div')
+    slide.setAttribute('class', 'slides');
+    img = document.createElement('img');
+    img.setAttribute('src', item);
+    slide.appendChild(img);
+    carousel.appendChild(slide);
+    slides.push(slide);
+    showSlides(slideNum, slides);
+    if (i != config.length - 1) {
+      nextButton = addNextButton(slide, slideNum, slides);
+      nextButton.addEventListener('click', function(){
+        showSlides(slideNum += 1, slides);
+      });
+    }
+    if (i != 0) {
+      prevButton = addPrevButton(slide, slideNum, slides);
+      prevButton.addEventListener('click', function(){
+        showSlides(slideNum -= 1, slides);
+      });
+    }
   });
+}
 
-  prevButton.forEach((itemPrevButton) => {
-    itemPrevButton.addEventListener('click', function(){
-      showSlides(slideNum -= 1, item, slides)
-    });
-  });
-});
+function addNextButton(slide, slideNum, slides) {
+  let nextButton = document.createElement('button');
+  nextButton.innerText = ">";
+  nextButton.setAttribute('class', 'next');
+  slide.appendChild(nextButton);
+  return nextButton;
+}
 
-function showSlides(slideNum, carouselNum, slides) {
+function addPrevButton(slide, slideNum, slides) {
+  let prevButton = document.createElement('button');
+  prevButton.innerText = "<";
+  prevButton.setAttribute('class', 'prev');
+  slide.appendChild(prevButton);
+  return prevButton;
+}
+
+function showSlides(slideNum, slides) {
   slides.forEach((item) => {
-    item.style.display = "none";
+    item.setAttribute('class', 'noDisplay');
   });
-  slides[slideNum - 1].style.display = "block";
+  slides[slideNum - 1].setAttribute('class', 'display');
 }
