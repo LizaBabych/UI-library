c580d255f74a5d563d250bfbbdb5394b6dbfdc75
LizaBabych <62441776+LizaBabych@users.noreply.github.com>
2020-06-13 20:53:24 +0300

    Add search without filters.

--- js/data-tables.js
+++ js/data-tables.js
@@ -5,18 +5,26 @@ const config1 = {
     {title: 'Имя', value: 'name'},
     {title: 'Фамилия', value: 'surname', sortable: true},
     {title: 'Возраст', value: 'age', type: 'number', sortable: true},
-  ]
+  ],
+  search: {
+    fields: ['name', 'surname'],
+    filters: [
+      v => v.toLowerCase(),
+      v => toKeyboardLayout(v, 'ru'),
+      v => toKeyboardLayout(v, 'en')
+    ]
+  }
 };
 
 const users = [
   {id: 30050, name: 'Вася', surname: 'Петров', age: 12},
-  {id: 30051, name: 'Вася', surname: 'Васечкин', age: 15},
-  {id: 30050, name: 'Вася', surname: 'Абрамов', age: 13},
-  {id: 30051, name: 'Вася', surname: 'Бровкин', age: 18},
-  {id: 30050, name: 'Вася', surname: 'Каролович', age: 11},
-  {id: 30051, name: 'Вася', surname: 'Мишкин', age: 10},
-  {id: 30050, name: 'Вася', surname: 'Цветочек', age: 19},
-  {id: 30051, name: 'Вася', surname: 'Иванов', age: 25},
+  {id: 30051, name: 'Василий', surname: 'Васечкин', age: 15},
+  {id: 30050, name: 'Иван', surname: 'Абрамов', age: 13},
+  {id: 30051, name: 'Максим', surname: 'Бровкин', age: 18},
+  {id: 30050, name: 'Кирилл', surname: 'Каролович', age: 11},
+  {id: 30051, name: 'Роман', surname: 'Мишкин', age: 10},
+  {id: 30050, name: 'Дмитрий', surname: 'Цветочек', age: 19},
+  {id: 30051, name: 'Руслан', surname: 'Иванов', age: 25},
 ];
 
 dataTable(config1, users);
@@ -24,19 +32,71 @@ dataTable(config1, users);
 function dataTable(config, users) {
   let table = document.createElement('table');
   let parent = document.querySelector(config.parent);
+  let searchForm = document.createElement('div');
+  let searchInput = document.createElement('input');
+  let searchLabel = document.createElement('label');
+  let row;
+  if(config.search) {
+    addSearchForm(parent, searchForm, searchInput, searchLabel);
+    let searchUser;
+    addInputListener(table, users, searchInput, config, searchUser);
+    addHeaderOfTable(table, config, row);
+  }
   parent.appendChild(table);
-  addHeaderOfTable(table, config);
   renderTable(usersTable.querySelector('table'), users);
 }
 
-function addHeaderOfTable(table, config) {
+function addInputListener(table, users, input, config, searchUser) {
+	input.addEventListener('input', () => {
+    let tbody = table.querySelector('tbody');
+    searchUser = findDefineElements(input.value.trim(), users, config.search.fields, config.search.filters);
+    tbody.remove();
+		renderTable(table, searchUser);
+	});
+}
+
+function findDefineElements(value, users, fields, filters) {
+  let searchUser = [];
+  if (value != "") {
+    users.forEach((itemUsers) => {
+      for (let index in itemUsers) {
+        fields.forEach((itemFields) => {
+          if(index == itemFields) {
+            filters.forEach((itemFilters) => {
+               if (itemUsers[index].itemFilters === value.itemFilters) {
+                 if (itemUsers[index].search(value) != -1 && searchUser.indexOf(itemUsers) == -1) {
+                   searchUser.push(itemUsers);
+                 }
+               }
+            });
+          }
+        });
+      }
+    });
+  } else return users;
+  return searchUser;
+}
+
+function addSearchForm(parent, searchForm, searchInput, searchLabel) {
+  searchForm.setAttribute('class', 'table-search');
+  searchInput.setAttribute('type', 'text');
+  searchInput.setAttribute('class', 'table-search-input');
+  searchInput.setAttribute('id', 'search');
+  searchLabel.innerText = "Search";
+  searchLabel.setAttribute('for', 'search');
+  searchLabel.setAttribute('class', 'table-search-label');
+  searchForm.appendChild(searchLabel);
+  searchForm.appendChild(searchInput);
+  parent.appendChild(searchForm);
+}
+
+function addHeaderOfTable(table, config, row) {
   let columns = config.columns;
 	const headTable = table.createTHead().insertRow(0);
-	let row;
 	columns.forEach((item) => {
 		row = headTable.appendChild(document.createElement('th'));
 		row.setAttribute('id', columns.value);
-    row.innerHTML = item.title;
+    row.innerText = item.title;
     if (item.type == true) row.setAttribute('class', 'align-right');
     addSortingButton(item, row, table);
 	});
