f64ac6948481e5674609d63df4490e6c8625cbca
LizaBabych <62441776+LizaBabych@users.noreply.github.com>
2020-06-13 13:52:12 +0300

    Change desicion.

--- js/burger.js
+++ js/burger.js
@@ -1,8 +1,19 @@
-function openBurgerMenu() {
-  document.getElementById("menu").style.display = 'block';
-  document.getElementById("close").style.display = 'block';
-}
+let headerBurger = document.querySelector(".header-burger");
+let menu = document.getElementById("menu");
+let openBurger = document.getElementById("openBurger");
+let closeBurger = document.createElement('i');
+closeBurger.setAttribute('class', 'fas fa-times');
+closeBurger.style.display = 'none';
+headerBurger.appendChild(closeBurger);
 
-function closeBurgerMenu() {
-  document.getElementById("menu").style.display = 'none';
-}
+openBurger.addEventListener('click', () => {
+  menu.setAttribute('class', 'header-menu-close');
+  openBurger.style.display = 'none';
+  closeBurger.style.display = 'block';
+});
+
+closeBurger.addEventListener('click', () => {
+  menu.setAttribute('class', 'header-menu');
+  closeBurger.style.display = 'none';
+  openBurger.style.display = 'block';
+});
