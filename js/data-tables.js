5af5f85bb1e74c8adda644bed24b3b97fb1962cc
LizaBabych <62441776+LizaBabych@users.noreply.github.com>
2020-06-18 21:28:12 +0300

    Back

--- js/data-tables.js
+++ js/data-tables.js
@@ -2,142 +2,47 @@ const config1 = {
   parent: '#usersTable',
   columns: [
     {title: '№', value: '_index'},
-    {title: 'Создано', value: 'createAt'},
     {title: 'Имя', value: 'name'},
-    {title: 'Аватар', value: 'avatar'},
     {title: 'Фамилия', value: 'surname', sortable: true},
-    {title: 'Возраст', value: (user) => calculateAge(user.birthday), type: 'age', sortable: true},
-  ],
-  search: {
-    fields: ['name', 'surname'],
-    filters: [
-      v => v.toLowerCase(),
-      v => toKeyboardLayout(v, 'ru'),
-      v => toKeyboardLayout(v, 'en')
-    ]
-  },
-  apiUrl: "https://5e938231c7393c0016de48e6.mockapi.io/api/ps5/players"
+    {title: 'Возраст', value: 'age', type: 'number', sortable: true},
+  ]
 };
 
 const users = [
   {id: 30050, name: 'Вася', surname: 'Петров', age: 12},
-  {id: 30051, name: 'Василий', surname: 'Васечкин', age: 15},
-  {id: 30050, name: 'Иван', surname: 'Абрамов', age: 13},
-  {id: 30051, name: 'Максим', surname: 'Бровкин', age: 18},
-  {id: 30050, name: 'Кирилл', surname: 'Каролович', age: 11},
-  {id: 30051, name: 'Роман', surname: 'Мишкин', age: 10},
-  {id: 30050, name: 'Дмитрий', surname: 'Цветочек', age: 19},
-  {id: 30051, name: 'Руслан', surname: 'Иванов', age: 25},
+  {id: 30051, name: 'Вася', surname: 'Васечкин', age: 15},
+  {id: 30050, name: 'Вася', surname: 'Абрамов', age: 13},
+  {id: 30051, name: 'Вася', surname: 'Бровкин', age: 18},
+  {id: 30050, name: 'Вася', surname: 'Каролович', age: 11},
+  {id: 30051, name: 'Вася', surname: 'Мишкин', age: 10},
+  {id: 30050, name: 'Вася', surname: 'Цветочек', age: 19},
+  {id: 30051, name: 'Вася', surname: 'Иванов', age: 25},
 ];
 
-dataTable(config1);
-
-//dataTable(config1, users);
+dataTable(config1, users);
 
 function dataTable(config, users) {
   let table = document.createElement('table');
   let parent = document.querySelector(config.parent);
-  let searchForm = document.createElement('div');
-  let searchInput = document.createElement('input');
-  let searchLabel = document.createElement('label');
-  let row;
-  if(config.search) {
-    addSearchForm(parent, searchForm, searchInput, searchLabel);
-    addInputListener(table, users, searchInput, config);
-    addHeaderOfTable(table, config, row, users);
-  }
   parent.appendChild(table);
+  addHeaderOfTable(table, config);
   renderTable(usersTable.querySelector('table'), users);
 }
 
-async function dataTable(config) {
-  let table = document.createElement('table');
-  let parent = document.querySelector(config.parent);
-  let searchForm = document.createElement('div');
-  let searchInput = document.createElement('input');
-  let searchLabel = document.createElement('label');
-  let row, users;
-  if(config.apiUrl){
-    users = await getDataFromURL(config.apiUrl);
-  }
-  if(config.search) {
-    addSearchForm(parent, searchForm, searchInput, searchLabel);
-    addInputListener(table, users, searchInput, config);
-    addHeaderOfTable(table, config, row, users);
-  }
-  parent.appendChild(table);
-  renderTable(usersTable.querySelector('table'), users);
-}
-
-async function getDataFromURL(url) {
-  try {
-    let response = await fetch(url, {method: "GET"});
-    if (!response.ok) {
-        console.log("Error: " + response.status);
-    }
-    return await response.json();
-  } catch (error) {
-      console.log("Error: " + error.message);
-  }
-}
-
-function addInputListener(table, users, input, config) {
-	input.addEventListener('input', () => {
-    let tbody = table.querySelector('tbody');
-    let searchUser = findDefineElements(input.value.trim(), users, config.search.fields, config.search.filters);
-    tbody.remove();
-		renderTable(table, searchUser);
-	});
-}
-
-function findDefineElements(value, users, fields, filters) {
-  let searchUser = [];
-  if (value != "") {
-    users.forEach((itemUsers) => {
-      for (let index in itemUsers) {
-        fields.forEach((itemFields) => {
-          if(index == itemFields) {
-            filters.forEach((itemFilters) => {
-               if (itemUsers[index].itemFilters === value.itemFilters) {
-                 if (itemUsers[index].search(value) != -1 && searchUser.indexOf(itemUsers) == -1) {
-                   searchUser.push(itemUsers);
-                 }
-               }
-            });
-          }
-        });
-      }
-    });
-  } else return users;
-  return searchUser;
-}
-
-function addSearchForm(parent, searchForm, searchInput, searchLabel) {
-  searchForm.setAttribute('class', 'table-search');
-  searchInput.setAttribute('type', 'text');
-  searchInput.setAttribute('class', 'table-search-input');
-  searchInput.setAttribute('id', 'search');
-  searchLabel.innerText = "Search";
-  searchLabel.setAttribute('for', 'search');
-  searchLabel.setAttribute('class', 'table-search-label');
-  searchForm.appendChild(searchLabel);
-  searchForm.appendChild(searchInput);
-  parent.appendChild(searchForm);
-}
-
-function addHeaderOfTable(table, config, row, data) {
+function addHeaderOfTable(table, config) {
   let columns = config.columns;
 	const headTable = table.createTHead().insertRow(0);
+	let row;
 	columns.forEach((item) => {
 		row = headTable.appendChild(document.createElement('th'));
 		row.setAttribute('id', columns.value);
-    row.innerText = item.title;
+    row.innerHTML = item.title;
     if (item.type == true) row.setAttribute('class', 'align-right');
-    addSortingButton(item, row, table, data);
+    addSortingButton(item, row, table);
 	});
 }
 
-function addSortingButton(item, row, table, data) {
+function addSortingButton(item, row, table) {
   for (let index in item) {
     if (index === 'sortable') {
       let buttonSortDown = document.createElement('button');
@@ -146,18 +51,18 @@ function addSortingButton(item, row, table, data) {
       addIconsToButton(buttonSortUp, buttonSortDown, buttonSort);
       row.appendChild(buttonSort);
       buttonSort.addEventListener('click', (e) =>{
-        sorting(table, item, 1, data);
+        sorting(table, item, 1);
         row.appendChild(buttonSortUp);
         buttonSort.remove();
       });
       buttonSortUp.addEventListener('click', (e) =>{
-        sorting(table, item, -1, data);
+        sorting(table, item, -1);
         buttonSortUp.remove();
         row.appendChild(buttonSortDown);
       });
       buttonSortDown.addEventListener('click', (e) =>{
         table.querySelector('tbody').remove();
-        renderTable(table, data);
+        renderTable(table, users);
         buttonSortDown.remove();
         row.appendChild(buttonSort);
       });
@@ -177,9 +82,9 @@ function addIconsToButton(buttonSortUp, buttonSortDown, buttonSort) {
   buttonSortDown.appendChild(iconDown);
 }
 
-function sorting(table, index, koef, data) {
+function sorting(table, index, koef) {
   let tbody = table.querySelector('tbody');
-  let sortData = [...data];
+  let sortData = [...users];
   if(index.type === 'number') {
     sortData.sort((u1, u2) => (u1.age - u2.age) * koef);
   } else {
@@ -193,12 +98,12 @@ function renderTable(table, data) {
   let tbody = table.appendChild(document.createElement('tbody'));
   let _index = 1;
   let j;
-  let row, cell;
   data.forEach((item, i) => {
+    let row, cell;
     j = 1;
-    row = tbody.insertRow(i);
-    cell = row.insertCell(0);
-    cell.innerHTML = _index++;
+		row = tbody.insertRow(i);
+		cell = row.insertCell(0);
+		cell.innerHTML = _index++;
     setTableAttributes(row, item, j);
   });
 }
@@ -207,17 +112,8 @@ function setTableAttributes(row, users, num) {
   for (let index in users) {
     if (index != 'id') {
       let cell = row.insertCell(num++);
-      if (typeof users[index] === 'number')
-        cell.setAttribute('class', 'align-right');
-      if (index == 'createdAt') {
-        cell.innerHTML = users[index].slice(0, 10);
-      } else if (index == 'avatar') {
-          let ava = document.createElement('img');
-          ava.setAttribute('src', users.avatar);
-          cell.appendChild(ava);
-      } else {
-          cell.innerHTML = users[index];
-      }
+      if (typeof users[index] === 'number') cell.setAttribute('class', 'align-right');
+      cell.innerHTML = users[index];
     }
   }
 }
