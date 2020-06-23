672744f3953b5fa7b21c7a0b77cf8eda1fac7479
LizaBabych <62441776+LizaBabych@users.noreply.github.com>
2020-06-23 16:03:28 +0300

    Update modal.js

--- js/modal.js
+++ js/modal.js
@@ -2,19 +2,21 @@ const send = document.querySelectorAll(".modal-trigger");
 const closeModal = document.querySelectorAll(".modal-close");
 let newButton;
 
-send.forEach((item, i) => {
-  item.addEventListener('click', function() {
-    item.nextElementSibling.style.display = "block";
-    newButton = document.createElement("button");
-    newButton.textContent = "X";
-    item.nextElementSibling.firstElementChild.firstElementChild.appendChild(newButton);
-    newButton.addEventListener('click', function(){closeModalWindow(closeModal[i])});
+function openModalWindow() {
+  send.forEach((item, i) => {
+    item.addEventListener('click', function() {
+      item.nextElementSibling.style.display = "block";
+      newButton = document.createElement("button");
+      newButton.textContent = "X";
+      item.nextElementSibling.firstElementChild.firstElementChild.appendChild(newButton);
+      newButton.addEventListener('click', function(){closeModalWindow(closeModal[i])});
+    });
   });
-});
 
-closeModal.forEach((item, i) => {
-  item.addEventListener('click', function(){closeModalWindow(closeModal[i])});
-});
+  closeModal.forEach((item, i) => {
+    item.addEventListener('click', function(){closeModalWindow(closeModal[i])});
+  });
+}
 
 function closeModalWindow(id) {
   id.parentElement.parentElement.parentElement.style.display = "none";
