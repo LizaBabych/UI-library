12b029e6af8f428604977497e263637aedda8dfc
Liza Babych <bettyhetfield11951@gmail.com>
2020-07-04 16:05:20 +0300

    Add master validation

--- js/input.js
+++ js/input.js
@@ -25,15 +25,26 @@ Vue.component('my-input', {
 			default: '',
 			required: true
 		},
+		pattern: {
+			type: RegExp,
+			required: false,
+		},
+		errormessage: {
+			type: String,
+			default: "It's required",
+			required: false
+		},
 	},
 	data() {
 		return {
 			invalidInput: false,
+			invalidExpression: false,
 		}
 	},
 	methods: {
 		correctMessage: function (event) {
 			this.invalidInput = this.required && event.target.value == "";
+			this.invalidExpression = !event.target.value.match(this.pattern)
 		},
 		setValid: function (val) {
       this["cl"] = true;
@@ -43,22 +54,24 @@ Vue.component('my-input', {
 	computed: {
 		dangerInput: function () {
 			return {
-				'input-danger': this.invalidInput
+				'input-danger': this.invalidInput || this.invalidExpression
 			};
 		},
 	},
 	template: `
 	<div class="my-input-form">
 		<label class="my-label"> {{title}} <span v-if="required">*</span>
-		<input class="my-input"
-		:class="dangerInput"
-    :placeholder="placeholder"
-		:required="required"
-		:type="type"
-		:value="value"
-		@blur="correctMessage"
-		@input="setValid($event.target.value)"
-		></input>
+			<input class="my-input"
+			:class="dangerInput"
+	    :placeholder="placeholder"
+			:required="required"
+			:type="type"
+			:value="value"
+			@blur="correctMessage"
+			@input="setValid($event.target.value)"
+			@focus="invalidExpression = false"
+			></input>
+		<span v-if="invalidExpression" class="invalidExpression">  {{errormessage}} </span>
 		</label>
 	</div>
 `
