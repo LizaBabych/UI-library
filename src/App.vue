be2edc16d06ac36228ec6d1e9130b3fd6c170a92
Liza Babych <bettyhetfield11951@gmail.com>
2020-07-08 13:06:54 +0300

    Add grid

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,64 @@
 <template>
   <div id="app">
-    <router-view/>
+    <Row>
+      <Column>1 of 1</Column>
+    </Row>
+    <Row>
+      <Column>1 of 2</Column>
+      <Column>2 of 2</Column>
+    </Row>
+    <Row>
+      <Column>1 of 3</Column>
+      <Column>2 of 3</Column>
+      <Column>3 of 3</Column>
+    </Row>
+    <Row>
+      <Column>1 of 4</Column>
+      <Column>2 of 4</Column>
+      <Column>3 of 4</Column>
+      <Column>4 of 4</Column>
+    </Row>
+    <Row>
+      <Column>1 of 6</Column>
+      <Column>2 of 6</Column>
+      <Column>3 of 6</Column>
+      <Column>4 of 6</Column>
+      <Column>5 of 6</Column>
+      <Column>6 of 6</Column>
+    </Row>
+    <Row>
+      <Column>1 of 13</Column>
+      <Column>2 of 12</Column>
+			<Column>3 of 12</Column>
+			<Column>4 of 12</Column>
+      <Column>5 of 12</Column>
+      <Column>6 of 12</Column>
+			<Column>7 of 12</Column>
+			<Column>8 of 12</Column>
+      <Column>9 of 12</Column>
+      <Column>10 of 12</Column>
+			<Column>11 of 12</Column>
+			<Column>12 of 12</Column>
+    </Row>
   </div>
 </template>
+
+<script>
+	import Row from '@/components/grid/Row.vue';
+	import Column from '@/components/grid/Column.vue';
+
+	export default {
+		components: {
+			Row,
+			Column,
+		},
+		data () {
+			return {
+				cols: '12'
+			};
+		},
+	}
+</script>
 
 <style lang="less">
 </style>
