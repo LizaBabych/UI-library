76d28605d57adee5d16288dc198db3f7c2c0fb8b
Liza Babych <bettyhetfield11951@gmail.com>
2020-07-10 15:56:33 +0300

    Add grid from App.vue to Home.vue

--- src/App.vue
+++ src/App.vue
@@ -1,64 +1,8 @@
 <template>
   <div id="app">
-    <Row>
-      <Column>1</Column>
-    </Row>
-    <Row>
-      <Column>2</Column>
-      <Column>2</Column>
-    </Row>
-    <Row>
-      <Column>3</Column>
-      <Column>3</Column>
-      <Column>3</Column>
-    </Row>
-    <Row>
-      <Column>4</Column>
-      <Column>4</Column>
-      <Column>4</Column>
-      <Column>4</Column>
-    </Row>
-    <Row>
-      <Column>6</Column>
-      <Column>6</Column>
-      <Column>6</Column>
-      <Column>6</Column>
-      <Column>6</Column>
-      <Column>6</Column>
-    </Row>
-    <Row>
-      <Column>12</Column>
-      <Column>12</Column>
-			<Column>12</Column>
-			<Column>12</Column>
-      <Column>12</Column>
-      <Column>12</Column>
-			<Column>12</Column>
-			<Column>12</Column>
-      <Column>12</Column>
-      <Column>12</Column>
-			<Column>12</Column>
-			<Column>12</Column>
-    </Row>
+    <router-view/>
   </div>
 </template>
 
-<script>
-	import Row from '@/components/grid/Row.vue';
-	import Column from '@/components/grid/Column.vue';
-
-	export default {
-		components: {
-			Row,
-			Column,
-		},
-		data () {
-			return {
-				cols: '12'
-			};
-		},
-	}
+<script lang="ts">
 </script>
-
-<style lang="less">
-</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,63 @@
 <template>
-  <div>
-    Vue
+  <div id="app">
+    <Row>
+      <Column>1</Column>
+    </Row>
+    <Row>
+      <Column>2</Column>
+      <Column>2</Column>
+    </Row>
+    <Row>
+      <Column>3</Column>
+      <Column>3</Column>
+      <Column>3</Column>
+    </Row>
+    <Row>
+      <Column>4</Column>
+      <Column>4</Column>
+      <Column>4</Column>
+      <Column>4</Column>
+    </Row>
+    <Row>
+      <Column>6</Column>
+      <Column>6</Column>
+      <Column>6</Column>
+      <Column>6</Column>
+      <Column>6</Column>
+      <Column>6</Column>
+    </Row>
+    <Row>
+      <Column>12</Column>
+      <Column>12</Column>
+			<Column>12</Column>
+			<Column>12</Column>
+      <Column>12</Column>
+      <Column>12</Column>
+			<Column>12</Column>
+			<Column>12</Column>
+      <Column>12</Column>
+      <Column>12</Column>
+			<Column>12</Column>
+			<Column>12</Column>
+    </Row>
   </div>
 </template>
 
 <script>
 
+import Row from '@/components/grid/Row.vue';
+import Column from '@/components/grid/Column.vue';
+
 export default {
   name: 'Home',
+  components: {
+    Row,
+    Column,
+  },
+  data () {
+    return {
+      cols: '12'
+    };
+  },
 }
 </script>
