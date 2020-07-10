c78d32db11c14779d7bf93c26ba3571f30e6afac
Liza Babych <bettyhetfield11951@gmail.com>
2020-07-10 15:52:46 +0300

    Add button from App.vue to Home.vue

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,8 @@
 <template>
   <div id="app">
-    <!-- <router-view/> -->
-    <MyButton v-for="i in todos"
-        :key="i.id"
-        :type="i.type"
-        :title="i.title"
-				:color="i.color"
-        :size="i.size"
-			>
-		</MyButton>
+    <router-view/>
   </div>
 </template>
 
 <script lang="ts">
-import MyButton from '@/components/MyButton.vue';
-
-export default({
-  components: {
-    MyButton,
-  },
-  data() {
-    return {
-      todos: [
-        { id: 1, title: "Large", color: "success", size: "lg"},
-        { id: 2, title: "Large", color: "primary", size: "lg"},
-        { id: 3, title: "Large", color: "secondary", size: "lg"},
-        { id: 4, title: "Middle", color: "warning"},
-        { id: 5, title: "Middle", color: "danger"},
-        { id: 6, title: "Default"},
-        { id: 7, title: "Small", color: "info", size: "sm"},
-        { id: 8, title: "Small", color: "light", size: "sm"},
-        { id: 9, title: "Small", color: "dark", size: "sm"},
-      ],
-    };
-  },
-});
 </script>
-
-<style lang="less">
-</style>
