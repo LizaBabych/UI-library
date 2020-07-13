e360942e0931ba8233dd3738733dcde92970fb17
Liza Babych <bettyhetfield11951@gmail.com>
2020-07-13 13:09:40 +0300

    Add carousel component

---
+++ src/components/MyCarousel.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <div class='slides'>
+      <img src='https://loremflickr.com/320/240/dog' alt='Error'>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue';
+  export default Vue.extend({
+    name: 'MyCarousel',
+    props: {
+      pictures: {
+        type: Array,
+        default: ['https://loremflickr.com/320/240/dog', 'https://loremflickr.com/g/320/240/paris',
+         'https://loremflickr.com/320/240/paris,girl/all', 'https://loremflickr.com/320/240/dog', 'https://loremflickr.com/g/320/240/paris',
+         'https://loremflickr.com/320/240/paris,girl/all', 'https://loremflickr.com/320/240/dog', 'https://loremflickr.com/g/320/240/paris',
+         'https://loremflickr.com/320/240/paris,girl/all'],
+      },
+    },
+    methods: {
+
+    },
+  });
+</script>
+
+<style lang="less">
+@button-color: rgba(159, 99, 71, 0.5);
+
+#slider {
+  margin-left: 38%;
+  position: relative;
+  & button {
+    background-color: @button-color;
+  }
+}
+
+.prev, .next  {
+  position: absolute;
+  cursor: pointer;
+  font-size: 18px;
+  margin-top: 12.5%;
+  transition: 0.6s ease;
+  border-radius: 0 3px 3px 0;
+}
+
+.prev { left: 0; }
+
+.next { left: 294px; }
+
+.noDisplay {display: none; }
+
+.display {display: block; }
+
+.prev:hover, .next:hover {
+  background-color: rgba(0, 0, 0, 0.8);
+  color: white;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,15 @@
 <template>
   <div>
-    Vue
+    <MyCarousel></MyCarousel>
   </div>
 </template>
 
-<script>
-
-export default {
-  name: 'Home',
-}
+<script lang="ts">
+  import MyCarousel from '@/components/MyCarousel.vue';
+  export default {
+    name: 'Home',
+    components: {
+      MyCarousel,
+    },
+  };
 </script>
