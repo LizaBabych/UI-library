92b9d63bf71f85d2baf36f3767ab1280a28c4d6c
Liza Babych <bettyhetfield11951@gmail.com>
2020-07-14 11:51:03 +0300

    Add table

--- src/components/MyDataTable.vue
+++ src/components/MyDataTable.vue
@@ -1,21 +1,100 @@
 <template>
-  <div>
-
+  <div id="usersTable">
+    <label class="table-search-label" for="table-search-input">Search</label>
+    <input id="table-search-input"
+      v-if="search"
+      type="text"
+      v-model="query"
+    >
+    <table>
+      <thead>
+      <tr>
+        <th
+          v-for="(col, index) in columns"
+          :key="index"
+          :class="{'align-right': col.type === 'number'}"
+        >
+          {{ col.title }}
+          <button
+            v-if="col.sortable"
+            @click="changeSort(col)"
+          >
+            <i :class="col.currentSortIcon"></i>
+          </button>
+        </th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr
+        v-for="(item, itemIndex) in sortedItems"
+        :key="itemIndex"
+      >
+        <td
+          v-for="(col, colIndex) in columns"
+          :key="colIndex"
+          :class="{'align-right': col.type === 'number'}"
+        >
+          {{ col.value === '_index' ? +itemIndex + 1 : item[col.value] }}
+        </td>
+      </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
-<script lang="ts">
+<script>
   import Vue from 'vue';
   export default Vue.extend({
     name: 'MyDataTable',
     props: {
-
+      items: {
+        type: Array,
+        required: true,
+      },
+      columns: {
+        type: Array,
+        required: true,
+      },
+      search: {
+        type: Object,
+        default: null,
+      },
+    },
+    data() {
+      return {
+        sortByColumn: null,
+        query: '',
+      };
     },
     methods: {
-
+      sortBy(items, column, query) {
+        if (query !== '') {
+          return items.filter((item) => {
+            return this.search.fields.filter((field) => {
+              return this.search.filters.filter((searchFilter) => {
+                return searchFilter(item[field] + '').includes(searchFilter(this.query));
+              }).length
+            }).length
+          });
+        }
+        if (column !== null) {
+          const copiedItems = [...items];
+          const coef = column.currentSortIcon.includes('up') ? 1
+            : column.currentSortIcon.includes('down') ? -1
+              : 0;
+          if (column.type === 'number') {
+            return copiedItems.sort((u1, u2) => (u1[column.value] - u2[column.value]) * coef);
+          } else {
+            return copiedItems.sort((u1, u2) => (u1[column.value].localeCompare(u2[column.value])) * coef);
+          }
+        }
+        return items;
+      }
     },
     computed: {
-
+      sortedItems() {
+        return this.sortBy(this.items, this.sortByColumn, this.query);
+      }
     },
   });
 </script>
@@ -27,29 +106,26 @@
 @tcell-hover-color: rgba(255, 99, 71, 0.4);
 
 #usersTable {
+  margin-left: 25%;
+  #table-search-input {
+    height: 20px;
+    width: 190px;
+    background-color: LightGray;
+    &:focus {
+      background-color: white;
+    }
+  }
+  .table-search-label {
+    margin-right: 25px;
+    font-size: 25px;
+  }
   font-family: 'Balsamiq Sans', cursive;
   .table-search {
     margin-left: 20%;
     display: flex;
     flex-direction: row;
-    .table-search-input {
-      height: 30px;
-      width: 350px;
-      background-color: LightGray;
-      &:focus {
-        background-color: white;
-      }
-    }
-    .table-search-label {
-      margin-right: 25px;
-      font-size: 30px;
-    }
-    .table-add-button {
-      margin-left: 20px;
-    }
   }
   & table {
-    margin-left: 20%;
     margin-top: 30px;
     margin-bottom: 30px;
     border-collapse: collapse;
@@ -76,22 +152,6 @@
     & .align-right { text-align: right; }
   }
 }
-
-.table-add-button {
-  margin: 0;
-}
-
-.modal-form {
-  display: flex;
-  align-items: flex-start;
-  flex-direction: column;
-  margin-left: 100px;
-  & input {
-    margin-bottom: 10px;
-    height: 20px;
-  }
-}
-
 
 @media (max-width: 767px) {
   table {
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,53 @@
 <template>
-  <MyDataTable>
-  </MyDataTable>
+  <div class="data-table-view">
+    <MyDataTable
+      :columns="columns"
+      :items="items"
+      :sort-button-style="sortButtonStyle"
+      :search="search"
+    />
+  </div>
 </template>
 
-<script lang="ts">
+<script>
+  import Vue from 'vue';
   import MyDataTable from '@/components/MyDataTable.vue';
-  export default {
+  const defaultSort = 'fas fa-sort';
+  export default Vue.extend({
     name: 'Home',
     components: {
       MyDataTable,
     },
-  };
+    data() {
+      return {
+        columns: [
+          {title: '№', value: '_index'},
+          {title: 'Имя', value: 'name'},
+          {title: 'Фамилия', value: 'surname', sortable: true, currentSortIcon: defaultSort},
+          {title: 'Возраст', value: 'age', type: 'number', sortable: true, currentSortIcon: defaultSort},
+        ],
+        items: [
+          {id: 30050, name: 'Вася', surname: 'Петров', age: 12},
+          {id: 30051, name: 'Вася', surname: 'Васечкин', age: 15},
+          {id: 30050, name: 'Вася', surname: 'Абрамов', age: 13},
+          {id: 30051, name: 'Вася', surname: 'Бровкин', age: 18},
+          {id: 30050, name: 'Вася', surname: 'Каролович', age: 11},
+          {id: 30051, name: 'Вася', surname: 'Мишкин', age: 10},
+          {id: 30050, name: 'Вася', surname: 'Цветочек', age: 19},
+          {id: 30051, name: 'Вася', surname: 'Иванов', age: 25},
+        ],
+        sortButtonStyle: {
+          default: defaultSort,
+          ascending: 'fas fa-sort-up',
+          descending: 'fas fa-sort-down',
+        },
+        search: {
+          fields: ['name', 'surname'],
+          filters: [
+            v => v.toLowerCase()
+          ]
+        },
+      };
+    },
+  });
 </script>
