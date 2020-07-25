b901f19ea7750a4661ef25e574139ee668e41a75
Liza Babych <bettyhetfield11951@gmail.com>
2020-07-25 13:50:21 +0300

    Add home page

--- js/modal.js
+++ js/modal.js
@@ -1,22 +1,27 @@
-const send = document.querySelectorAll(".modal-trigger");
-const closeModal = document.querySelectorAll(".modal-close");
-let newButton;
+//addModalForm();
 
-send.forEach((item, i) => {
-  item.addEventListener('click', function() {
-    item.nextElementSibling.style.display = "block";
-    newButton = document.createElement("button");
-    newButton.textContent = "X";
-    item.nextElementSibling.firstElementChild.firstElementChild.appendChild(newButton);
-    newButton.addEventListener('click', function(){closeModalWindow(closeModal[i])});
+function addModalForm() {
+  const send = document.querySelectorAll(".modal-trigger");
+  const closeModal = document.querySelectorAll(".modal-close");
+  const newButton = document.createElement("button");
+  newButton.textContent = "X";
+  let xButton = document.querySelectorAll(".modal-head");
+  let modal;
+
+  send.forEach((item, i) => {
+    item.addEventListener('click', (e) => {
+      item.nextElementSibling.firstElementChild.firstElementChild.appendChild(newButton);
+      modal = document.getElementById(item.dataset.target);
+      modal.style.display = "block";
+      newButton.addEventListener('click', (e) => {
+        modal.style.display = "none";
+      });
+    });
   });
-});
 
-closeModal.forEach((item, i) => {
-  item.addEventListener('click', function(){closeModalWindow(closeModal[i])});
-});
-
-function closeModalWindow(id) {
-  id.parentElement.parentElement.parentElement.style.display = "none";
-  newButton.remove();
+  closeModal.forEach((item, i) => {
+    item.addEventListener('click', (e) => {
+      modal.style.display = "none";
+    });
+  });
 }
